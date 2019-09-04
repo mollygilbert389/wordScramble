f2f63c5e7ffbb0651bb3330ee6bb2ff48d2c38d2
Molly Gilbert <msgilbert389@gmail.com>
2019-09-04 15:22:21 -0500

    adding updates score functionality so now that form clears and score does not update when players click submit

--- assets/app.js
+++ assets/app.js
@@ -20,22 +20,21 @@ $(document).ready(function () {
     let chosenWord = "";
     let playerAScore = 0;
     let playerBScore = 0;
-    let playerA = "";
-    let playerB = "";
-    const database = firebase.database()
+    // let playerA = "";
+    // let playerB = "";
+    const database = firebase.database();
 
     //GAME START
     $("#userNameModal").show();
-    $("#userEnter").on("click", function () {
+    $("#userEnter").on("click", function() {
         myDisplayName = $("#userName").val().trim();
         if (myDisplayName != "") {
             $("#userNameModal").hide();
             $("#linkdiv").empty();
             createGame();
-        }
-        else {
-            alert("Please enter a username to play");
-        }
+            } else {
+                alert("Please enter a username to play");
+                };
 
     });
     $("#playModal").show();
@@ -45,32 +44,50 @@ $(document).ready(function () {
     //////////////////////////////////////////////////////////////////////////////////////////////
 
     function createGame() {
-        $("#createGame").on("click", function () {
+        $("#createGame").on("click", function() {
             database.ref().set({
                 players : {
                 name: myDisplayName,
                 opponent: "empty",
                 playerNum: "1"
                 }
+                });
+
+                loading();
             });
 
-            loading()
-        });
-
         database.ref().on("value", function (snap) {
-            $("#linkdiv").empty()
+            $("#linkdiv").empty();
             $("#linkdiv").append("<button class='clickyGame' data-name='" + snap.val().players.name + "'" + ">" + snap.val().players.name + "'s Game " + "</button>");
 
             $(".clickyGame").on("click", function () {
-                database.ref("/players").child("opponent").set(myDisplayName)
-                database.ref("/players").child("playerNum").set("2")
-                loading()
+                database.ref("/players").child("opponent").set(myDisplayName);
+                database.ref("/players").child("playerNum").set("2");
+                loading();
+                });
             });
-        });
-    };
+        };
+
+        function chooseWord() {
+            let chosenWord = letterBank[Math.floor(Math.random() * letterBank.length)];
+            chosenWord = chosenWord.toLowerCase();
+            database.ref("word/chosenWord").set(chosenWord)
+            console.log(chosenWord)
+    
+            scrambledWord = chosenWord.split("");
+            scrambledWord = scrambledWord.sort(function () { return 0.5 - Math.random() }).join('');
+            database.ref("word/scrambledWord").set(scrambledWord);
+            console.log(scrambledWord)
+    
+            database.ref("word").on("value", function(snap) {
+                $("#displayBox").empty();
+                let scrambledWord = snap.child("scrambledWord").val();
+                $("#displayBox").append(scrambledWord);
+                });
+            };
 
     function playGame() {
-        let gameTime = 10;
+        let gameTime = 60;
         $("#userGuessBox").show();
         $("#directions").empty();
         $("#directions").append("GO!");
@@ -80,74 +97,63 @@ $(document).ready(function () {
         database.ref().child("score").set({
             oneScore: 0,
             twoScore: 0
-        })
+            });
 
         newTimer = setInterval(gameCountdown, 1000);
+
         function gameCountdown() {
             gameTime--;
-            checkScore()
+            checkScore();
             $("#timer").html("Time Left in the Game: " + gameTime);
             if (gameTime <= 0) {
                 clearInterval(newTimer);
                 showWinner();
+                };
             };
-        };
 
         $(".wordGuess").on("click", function () {   
-            let playerAguess = $("#playerAGuess").val().trim().toLowerCase();
-            let playerBguess = $("#playerBGuess").val().trim().toLowerCase();
-            let frmA = document.getElementsByName('gameFormA')[0];
-            let frmB = document.getElementsByName('gameFormB')[0];
-          
-            if (playerAguess === chosenWord) {
-                playerAScore++
-                chooseWord()
-                database.ref("/score").child("oneScore").set(playerAScore)
-                frmA.reset();
-            } else {
-                frmA.reset();
-            };
-    
-            if (playerBguess === chosenWord) {
-                playerBScore++
-                chooseWord();
-                database.ref("/score").child("twoScore").set(playerBScore)
-                frmB.reset();
-    
-            } else {
-                frmB.reset();
-            };
-    
-        });
+            database.ref("word").on("value", function(snap) {
+                let chosenWord = snap.child("chosenWord").val();
+                console.log(chosenWord)
 
-    };
+                let playerAguess = $("#playerAGuess").val().trim().toLowerCase();
+                let playerBguess = $("#playerBGuess").val().trim().toLowerCase();
+                let frmA = document.getElementsByName('gameFormA')[0];
+                let frmB = document.getElementsByName('gameFormB')[0];
+
+                if (playerAguess === chosenWord) {
+                    playerAScore++;
+                    chooseWord();
+                    database.ref("/score").child("oneScore").set(playerAScore);
+                    frmA.reset();
+                    } else {
+                        frmA.reset();
+                        };
+        
+                if (playerBguess === chosenWord) {
+                    playerBScore++;
+                    chooseWord();
+                    database.ref("/score").child("twoScore").set(playerBScore);
+                    frmB.reset();
+                    } else {
+                        frmB.reset();
+                        };
+                });
+            });
+
+        };
 
     function checkScore() {
         database.ref("score").on("value", function(snap) {
             $("#playerAscore").empty();
             $("#playerBscore").empty();
-            let ascore = snap.child("twoScore").val();
-            let bscore = snap.child("oneScore").val();
+            let ascore = snap.child("oneScore").val();
+            let bscore = snap.child("twoScore").val();
             $("#playerAscore").append("Score: " + ascore);
             $("#playerBscore").append("Score: " + bscore);
-        })
-    }
+            });
+        };
 
-    function chooseWord() {
-        let chosenWord = letterBank[Math.floor(Math.random() * letterBank.length)];
-        console.log(chosenWord)
-        chosenWord = chosenWord.toLowerCase();
-        scrambledWord = chosenWord.split("");
-        scrambledWord = scrambledWord.sort(function () { return 0.5 - Math.random() }).join('');
-        
-        database.ref().child("chosenWord").set(scrambledWord)
-
-        database.ref("chosenWord").on("value", function(snap) {
-            $("#displayBox").empty();
-            let chosenWord = snap.val()
-            $("#displayBox").append(chosenWord);
-            })
-    };
 
     function loading() {
         database.ref("/players").on("value", function(snap) {
@@ -166,11 +172,11 @@ $(document).ready(function () {
                 $("#playerANameBox").append(playerOneName + "'s Guess Box");
 
             } else if (playerReady === "2") {
-                $("#directions").empty()
-                $("#playerANameBox").empty()
-                $("#directions").append("You have entered the game! You will now have 10 seconds to prepare. Rules: you will be competing with another play to guess the correct word from the scrambled letters below. Once a player guesses the correct word a new word will appear. Good Luck!")
+                $("#directions").empty();
+                $("#playerANameBox").empty();
+                $("#directions").append("You have entered the game! You will now have 10 seconds to prepare. Rules: you will be competing with another play to guess the correct word from the scrambled letters below. Once a player guesses the correct word a new word will appear. Good Luck!");
                 $("#playerANameBox").append(playerOneName + "'s Guess Box");
-                $("#playerBNameBox").empty()
+                $("#playerBNameBox").empty();
                 $("#playerBNameBox").append(playerTwoName + "'s Guess Box");
                 $("#playModal").hide();
                 let counter = 10;
@@ -184,36 +190,32 @@ $(document).ready(function () {
                         playGame();
                         };
                     };
-            }
+                };
 
-        });
+            });
 
-    };
+        };
 
     function showWinner() {
         modal = $("#myModal");
         message = $("#modalWinner");
         message.empty();
         database.ref().on("value", function(snap) {
-            let ascore = snap.child("score/twoScore").val();
-            let bscore = snap.child("score/oneScore").val();
+            let ascore = snap.child("score/oneScore").val();
+            let bscore = snap.child("score/twoScore").val();
             let OGname = snap.child("players/name").val();
             let opponentName = snap.child("players/opponent").val();
-            console.log(ascore)
-            console.log(bscore)
-            console.log(OGname)
-            console.log(opponentName)
 
             if (ascore > bscore) {
-                message.append("We have a winner!" + OGname + " Final Score: " + ascore);
+                message.append("We have a winner! " + OGname + " Final Score: " + ascore);
             }
             if (bscore > ascore) {
-                message.append("We have a winner!" + opponentName + " Final Score: " + bscore);
+                message.append("We have a winner! " + opponentName + " Final Score: " + bscore);
             }
             if (bscore === ascore) {
                 message.append("There was a tie!");
-            }
-        })
+            };
+        });
 
         modal.show();
         $("#playAgain").on("click", function () {
